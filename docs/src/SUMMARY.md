a111698cdca1857cd5ea9eba64f892716947c1d3
Cappy Ishihara <cappy@cappuchino.xyz>
2022-11-18 00:03:41 +0700

    update

---
+++ docs/src/SUMMARY.md
@@ -0,0 +1,5 @@
+# Summary
+
+- [Chapter 1](./1_about.md)
+- [Internal Structure](./2_internal_structure.md)
+    - [The NCN Format](./2.1_ncn_format.md)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{error::Error, fs::File, io::Read};
+
 use log::LevelFilter;
+use encoding::{all::WINDOWS_874, DecoderTrap, Encoding};
 
 fn main() {
     pretty_env_logger::formatted_builder()
@@ -7,4 +10,17 @@ fn main() {
 
     log::debug!("debug");
     println!("Hello, world!");
+
+    println!("{}", load_lyrics().unwrap());
+}
+
+
+fn load_lyrics() -> Result<String, Box<dyn Error>> {
+    let mut file = File::open("30664.LYR")?;
+    // autodetect encoding
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf)?;
+    // decode to utf8
+    let lyrics = WINDOWS_874.decode(&buf, DecoderTrap::Strict)?;
+    Ok(lyrics)
 }
